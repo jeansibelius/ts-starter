ae188418e971e7b0a2274020a5d4a22f5f760521
Rafael Linnankoski <rafaellinnankoski@gmail.com>
2024-07-01 14:19:31 +0900

    Run prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,10 @@
 /* eslint-env node */
 module.exports = {
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended-type-checked", "prettier"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "prettier",
+  ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: true,
